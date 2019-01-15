21d2ed6c3b6c09b41b83f983d4306ef5d3cc3048
Kevin Biddle <kdbiddle08@gmail.com>
2019-01-15 16:48:00 -0500

    Updates

---
+++ precache-manifest.7810b4fa4e875bfcbd9b304da2655196.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ff18f8ffe2539a1971c6",
+    "url": "/static/js/main.ff18f8ff.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "ff18f8ffe2539a1971c6",
+    "url": "/static/css/main.08cb1137.chunk.css"
+  },
+  {
+    "revision": "0f2d7cf9ae2299154311acd25d19bc01",
+    "url": "/index.html"
+  }
+];
